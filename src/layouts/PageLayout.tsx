62b0516b1b2cc5a39deb6bc7f3b16c314eed312c
Manuel Salvador <manu.sacr@hotmail.com>
2023-07-10 20:55:49 -0300

    og updates

--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -8,6 +8,10 @@ type Props = {
   className?: string;
 };
 
+const pageUrl = !!process.env.NEXT_PUBLIC_PREVIEW_MODE
+  ? 'https://preview-manuel-salvador.vercel.app'
+  : 'https://manuel-salvador.vercel.app';
+
 export default function PageLayout({ title, children, className }: Props) {
   return (
     <>
@@ -22,6 +26,8 @@ export default function PageLayout({ title, children, className }: Props) {
           content="Welcome to my portfolio! Explore my projects and discover my passion for creating innovative websites. With skills in web development, programming, and problem-solving, I'm ready to tackle new challenges. Get to know me and find out how I can bring value to your team!"
         />
         <meta property="og:image" content="https://i.imgur.com/vk5Dw1U.png" />
+        <meta property="og:url" content={pageUrl} />
+        <meta property="og:type" content="website" />
       </Head>
       <Header />
       <main className={className}>{children}</main>
