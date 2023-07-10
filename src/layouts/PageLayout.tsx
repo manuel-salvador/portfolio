03d2c6caa6ad49707e1e61a12185d63a65f8672e
Manuel Salvador <manu.sacr@hotmail.com>
2023-07-10 20:45:51 -0300

    og update

--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -16,6 +16,12 @@ export default function PageLayout({ title, children, className }: Props) {
         <meta name="description" content="Manuel Salvador portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content="Manuel Salvador | Full Stack Developer" />
+        <meta
+          property="og:description"
+          content="¡Bienvenido a mi portfolio! Explora mis proyectos y descubre mi pasión por crear webs innovadoras. Con habilidades en desarrollo web, programación y resolución de problemas, estoy listo para enfrentar nuevos desafíos. ¡Conóceme y descubre cómo puedo aportar valor a tu equipo!"
+        />
+        <meta property="og:image" content="https://i.imgur.com/vk5Dw1U.png" />
       </Head>
       <Header />
       <main className={className}>{children}</main>
