6701c36d73e46214ecbf300fd8bfced4edaa49d2
Manuel Salvador <manu.sacr@hotmail.com>
2023-07-10 21:07:02 -0300

    og updates

--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -20,14 +20,29 @@ export default function PageLayout({ title, children, className }: Props) {
         <meta name="description" content="Manuel Salvador portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <meta property="og:title" content="Manuel Salvador | Full Stack Developer" />
+        <meta property="og:title" content={title} />
         <meta
           property="og:description"
           content="Welcome to my portfolio! Explore my projects and discover my passion for creating innovative websites. With skills in web development, programming, and problem-solving, I'm ready to tackle new challenges. Get to know me and find out how I can bring value to your team!"
         />
         <meta property="og:image" content="https://i.imgur.com/vk5Dw1U.png" />
+        <meta property="og:image:secure_url" content="https://i.imgur.com/vk5Dw1U.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:alt" content="manuelSalvador" />
+        <meta property="og:image:type" content="image/png" />
         <meta property="og:url" content={pageUrl} />
+        <meta property="og:site_name" content="ManuelSalvadorPortfolio" />
         <meta property="og:type" content="website" />
+        <meta property="og:locale" content="en_US" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={title} />
+        <meta
+          name="twitter:description"
+          content="Welcome to my portfolio! Explore my projects and discover my passion for creating innovative websites. With skills in web development, programming, and problem-solving, I'm ready to tackle new challenges. Get to know me and find out how I can bring value to your team!"
+        />
+        <meta name="twitter:image" content="https://i.imgur.com/vk5Dw1U.png" />
+        <meta name="twitter:creator" content="@manu_svd" />
       </Head>
       <Header />
       <main className={className}>{children}</main>
